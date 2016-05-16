a9c8010d0418a64abad9dd8bf6e46c5f777f62b9
Mark McCahill <mark.mccahill@duke.edu>
2016-05-16 13:55:21 -0400

    simplify trhe Reed devtools install
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- conf/install-reed.R
+++ conf/install-reed.R
@@ -1,5 +1,3 @@
-r <- getOption("repos")
-r["CRAN"] <- "http://cran.r-project.org"
-options(repos=r)
-devtools::install_github('ismayc/reedoilabs')
-devtools::install_github('ismayc/reedtemplates')
+
+devtools::install_github("ismayc/reedoilabs")
+devtools::install_github("ismayc/reedtemplates")
