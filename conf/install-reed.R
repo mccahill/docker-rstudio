d2fc217064bf0812273af5005a3a9da5d6eb26b3
Mark McCahill <mark.mccahill@duke.edu>
2016-05-17 09:27:14 -0400

    tufte package install
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- conf/install-reed.R
+++ conf/install-reed.R
@@ -1,4 +1,7 @@
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.r-project.org"
+options(repos=r)
 
 devtools::install_github("ismayc/reedoilabs")
 devtools::install_github("ismayc/reedtemplates")
-install.packages("tufte")
+utils::install.packages("tufte")
